07d878110d573ec01d3fd0c6388ebf62ccb1afe6
mahmudul <mahmudul35-218@diu.edu.bd>
2025-10-30 16:45:39 +0600

    create navber section

--- components/navber.tsx
+++ components/navber.tsx
@@ -2,26 +2,26 @@
 import { useState } from "react";
 import Image from "next/image";
 import { FiChevronDown, FiMenu } from "react-icons/fi";
+import { IoMenuSharp } from "react-icons/io5";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-[#d8c7b2] px-6 md:px-16 py-3 flex items-center justify-between shadow-sm">
+    <nav className="bg-[#E0D1BE] px-6 md:px-16 py-3 flex items-center justify-between shadow-sm h-[98px]">
       {/* Left: Logo */}
       <div className="flex items-center space-x-2">
         <Image
-          src="/logoipsum-logo.png" // Replace with your logo path
+          src="/logo/logo.png" // Replace with your logo path
           alt="Logo"
-          width={35}
-          height={35}
+          width={188}
+          height={32}
         />
-        <span className="font-semibold text-gray-800 text-sm md:text-base">Logoipsum</span>
       </div>
 
       {/* Middle: Nav Links */}
       <div className="hidden md:flex items-center space-x-6 text-sm text-gray-800">
-        <button className="border border-gray-700 rounded-full px-3 py-1 hover:bg-gray-200 transition">
+        <button className="border border-gray-700 rounded-full px-3 py-1.5 hover:bg-gray-200 transition">
           HOME
         </button>
 
@@ -37,8 +37,8 @@ export default function Navbar() {
       </div>
 
       {/* Right: Contact + Menu */}
-      <div className="flex items-center space-x-3">
-        <button className="bg-[#0c2a1f] text-white px-4 py-2 rounded-full text-xs md:text-sm hover:bg-[#10382a] transition">
+      <div className="flex items-center space-x-6">
+        <button className="bg-[#122620] text-white px-4 py-2 rounded-full text-xs md:text-sm hover:bg-[#10382a] transition">
           CONTACT US
         </button>
         <button
@@ -47,13 +47,17 @@ export default function Navbar() {
         >
           <FiMenu />
         </button>
+
+        <div className="border rounded-full p-2">
+          <IoMenuSharp className="text-2xl" />
+        </div>
       </div>
 
       {/* Mobile Dropdown */}
       {menuOpen && (
         <div className="absolute top-14 right-6 bg-white border rounded-md shadow-md p-4 md:hidden">
           <div className="flex flex-col space-y-3 text-gray-800">
-            <button className="border border-gray-700 rounded-full px-3 py-1">
+            <button className="border border-gray-700 rounded-full px-3 py-1.5">
               HOME
             </button>
             <span className="cursor-pointer">SERVICES</span>
