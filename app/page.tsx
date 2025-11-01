396b79b799e03c5727378f9387a7b34daf6662e4
mahmudul <mahmudul35-218@diu.edu.bd>
2025-11-01 18:16:31 +0600

    finished project

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 "use client";
 
 
-import NextHeroWithFramerMotion from "@/components/HeroSection";
 import HeroSection from "@/components/HeroSection";
 import Publications from "@/components/proud";
 
-
-
 export default function Home() {
-  // const [expanded, setExpanded] = useState(false);
+  
 
   return (
     <div className=" ">
-  
-      {/* <Hero expanded={expanded} setExpanded={setExpanded} /> */}
-      {/* <HeroSection></HeroSection> */}
-         <NextHeroWithFramerMotion></NextHeroWithFramerMotion>
+      <HeroSection></HeroSection>
       <Publications></Publications>
-   
     </div>
   );
 }
